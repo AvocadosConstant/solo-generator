f360a4156e94816dab44c96dae9c3c9b82056627
Tim <thung1@binghamton.edu>
2015-11-26 14:17:38 -0500

    Implement basic fret clustering

--- main.js
+++ main.js
@@ -1,35 +1,52 @@
 //var tonic = "C";
 //var mode = "ionian";
 
-var strings = ["e", "B", "G", "D", "A", "E"];
-var tab = {
-    e: "|--",
-    B: "|--",
-    G: "|--",
-    D: "|--",
-    A: "|--",
-    E: "|--"
+var MAX_STRING_LENGTH = 50;
+var STRINGS = ["e", "B", "G", "D", "A", "E"];
+var TAB_TEMPLATE = {
+    e: "e|-",
+    B: "B|-",
+    G: "G|-",
+    D: "D|-",
+    A: "A|-",
+    E: "E|-"
 }
+var lastFret = 9;
+var tab = $.extend({}, TAB_TEMPLATE);;
 
 function newNote() {
-    var string = strings[Math.floor(Math.random() * 6)];
-    var fret = Math.floor(Math.random() * 10);
+    if(tab["e"].length >= MAX_STRING_LENGTH) return;
+    var string = STRINGS[Math.floor(Math.random() * 6)];
+    
+    var minFret = (lastFret - 4 < 0) ? 0 : lastFret - 3;
+    var fret = (lastFret - 4 < 0) ? Math.floor(Math.random() * 5) : lastFret-3 + Math.floor(Math.random() * 6);
+    lastFret = fret;
     console.log("String " + string + ", fret " + fret);
-    strings.forEach(function(entry) {
+    STRINGS.forEach(function(entry) {
         //console.log(entry);
         if(entry == string) tab[entry] += fret + "-";
         else tab[entry] += "--";
     });
+    printTab();
 }
 
 function printTab() {
     $("#tabs").empty();
-    strings.forEach(function(entry) {
+    STRINGS.forEach(function(entry) {
         $("#tabs").append(tab[entry] + "<br>");
     });
 }
 
-$("#genNote").click(function() {
-    newNote();
+function clearTab() {
+    tab = $.extend({}, TAB_TEMPLATE);
     printTab();
+}
+
+$("#genNote").click(function() {
+    //setInterval(newNote, 1000);
+    newNote();
+});
+
+$("#clearTabBtn").click(function() {
+    clearTab();
 });
